8bf63ce85b1ce82eb8ce625e2958369e116db17d
Deshawn Nelson <Deshawn.a.nelson@gmail.com>
2021-12-10 16:14:21 -0500

    feat: finalize app features based on the project specifications

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,112 @@
-let library = [
-
+const library = [
+    {author: "J.K. Rowling", title: "Harry Potter and The Sorcerer's Stone", pages: 223, read: "Haven't read it"},
+    {author: "George R.R. Martin", title: "A Game of Thrones", pages: "694", read: "Haven't read it"},
 ];
+const table = document.querySelector("table");
+updateBooks();
 
-displayBooks();
+function Book(author, title, pages, read) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
 
-function addBookToLibrary(book) {
+    read = read.toLowerCase();
+    if (read === "yes") {
+        this.read = "Read it";
+    } else if (read === "no") {
+        this.read = "Haven't read it";
+    }
+}
+
+function addBookToLibrary() {
+    book = promptForBook();
     library.push(book);
+    updateBooks();
 }
 
-function displayBooks() {
-    const body = document.querySelector("body");
-    let displayContainer = createTable();
+function promptForBook() {
+    const author = prompt("Author", "George R.R. Martin");
+    const title = prompt("Title", "A Game of Thrones");
+    const pages = prompt("pages", 694);
+    const read = prompt("Read?", "no");
 
-    library.forEach(book => {
-        const entry = document.createElement("tr");
-        
-        for (const prop in book) {
-            const item = createWithTextContent("td", book[prop]);
-            entry.appendChild(item);
-        }
+    return new Book(author, title, pages, read);
+}
 
-        displayContainer.appendChild(entry);
+const addButton = document.querySelector('[data-add-button]');
+addButton.addEventListener('click', () => {
+    addBookToLibrary();
+})
+
+function updateBooks() {
+    const container = document.querySelector(".container");
+    container.innerHTML = "";
+
+    library.forEach((book, index) => {
+        const div = document.createElement("div");
+        div.classList.add("book");
+
+        const titleDiv = createElementWithTextContent("div", book.title);
+        titleDiv.addEventListener('click', () => {
+            changeBookProperty("title", titleDiv.textContent, index);
+        })
+
+        const authorDiv = createElementWithTextContent("div", `by ${book.author}`);
+        authorDiv.addEventListener('click', () => {
+            const authorName = authorDiv.textContent.slice(3);
+            changeBookProperty("author", authorName, index);
+        })
+
+        const pagesDiv = createElementWithTextContent("div", `${book.pages} pages`);
+        pagesDiv.addEventListener('click', () => {
+            const pages = pagesDiv.textContent.split(" ")[0];
+            changeBookProperty("pages", +pages, index);
+        });
+        const readDiv = createElementWithTextContent("div", book.read);
+        readDiv.setAttribute("data-read", '');
+
+        readDiv.addEventListener('click', () => {
+            
+        })
+
+        const deleteButton = createElementWithTextContent("button", "Remove Book");
+        deleteButton.addEventListener('click', () => {
+            library.splice(index, 1);
+            updateBooks();
+        })
+
+        const readStatusButton = createElementWithTextContent("button", "Change Read Status");
+        readStatusButton.addEventListener('click', () => {
+            let readStatus = library[index].read;
+            library[index].read = (readStatus.toLowerCase() === "haven't read it") ? "Read it" : "Haven't read it" 
+            updateBooks();
+        })
+
+        div.appendChild(titleDiv);
+        div.appendChild(authorDiv);
+        div.appendChild(pagesDiv);
+        div.appendChild(readDiv);
+
+        const buttons = document.createElement("div");
+        buttons.appendChild(deleteButton);
+        buttons.appendChild(readStatusButton);
+
+        div.appendChild(buttons)
+        container.appendChild(div);
     })
-    body.appendChild(displayContainer);
 }
 
-function createTable() {
-    const fields = [
-        "author", "name", "pages", "read"
-    ]
-    const table = document.createElement("table");
-    const tr = document.createElement("tr");
-    fields.forEach(field => {
-        const item = createWithTextContent("th", field);
-        tr.appendChild(item);
-    });
-    table.appendChild(tr);
-    return table;
-}
-
-function createWithTextContent(type, text) {
-    const element = document.createElement(type);
+function createElementWithTextContent(tagName, text) {
+    const element = document.createElement(tagName);
     element.textContent = text;
 
     return element;
 }
+
+function changeBookProperty(property, currentValue, index) {
+    const newValue = prompt(`Enter new ${property}`, currentValue);
+
+    library[index][property] = (newValue != null) ? newValue : currentValue;
+
+    updateBooks();
+} 
